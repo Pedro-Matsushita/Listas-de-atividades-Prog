f9a3370bf60588bb6ddaab372068cc0bd14dcea0
Pedro-Matsushita <137213776+Pedro-Matsushita@users.noreply.github.com>
2023-10-25 13:38:04 +0000

    Mudança Q3 25/10

--- Lista 3 Java (matrizes)/Q3.java
+++ Lista 3 Java (matrizes)/Q3.java
@@ -12,19 +12,33 @@ public class Q3
     static Scanner ler = new Scanner(System.in);
     public static void main(String[] args){
         double m[][] = new double[10][10];
-
+        
+        //Entrada dos valores com gerador
         entradaMatriz(m);
 
+        //Exibição matriz original
         System.out.println("Exibição da matriz:\n");
+        exibeMatriz(m);
+        System.out.println();
+
+        //Chamada dos métodos de troca
+        trocarLinha(m, 2, 8);
+        trocarColuna(m, 4, 9);
+        trocarDiagonal(m);
+        trocarLinhaColuna(m, 5, 9);
+        
+        //Exibição da matriz após troca
+        System.out.println("Exibição da matriz após trocas:\n");
         exibeMatriz(m);
     }
     public static void entradaMatriz(double m[][]){
         Random r = new Random();
         for(int i = 0; i<m.length; i++){
             for(int j = 0; j<m[i].length; j++){
-                System.out.println("Matriz ["+ (i+1) +"] preenchida.");
+                System.out.println("Matriz ["+ (i+1) +"] [" + (j+1) + "] preenchida.");
                 m[i][j]= r.nextInt(15);
             }
+            System.out.println();
         }
     }
     public static void exibeMatriz(double m[][]){
@@ -33,6 +47,34 @@ public class Q3
                 System.out.print(m[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+    public static void trocarLinha(double m[][], int linha1, int linha2){
+        for(int j = 0; j < m.length; j++){
+            double temp = m[linha1][j];
+            m[linha1][j] = m[linha2][j];
+            m[linha2][j] = temp;
+        }
+    }
+    public static void trocarColuna(double m[][], int coluna1, int coluna2){
+        for(int i = 0; i < m.length; i++){
+            double temp = m[i][coluna1];
+            m[i][coluna1] = m[i][coluna2];
+            m[i][coluna2] = temp;
+        }
+    }
+    public static void trocarDiagonal(double m[][]){
+        for(int i = 0; i < m.length; i++){
+            double temp = m[i][i];
+            m[i][i] = m[m.length -i -1][i];
+            m[m.length -i  -1][i] = temp;
+        }
+    }
+    public static void trocarLinhaColuna(double m[][], int linha, int coluna){
+        for(int i = 0; i < m.length; i++){
+            double temp = m[i][linha];
+            m[i][linha] = m[i][coluna];
+            m[i][coluna] = temp;
         }
     }
 }
